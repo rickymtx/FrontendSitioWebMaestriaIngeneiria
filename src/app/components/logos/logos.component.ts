49cb9ebeaa9ab34467710815aa71ec2e39a147cc
rickymtx <rich.mtx1205@gmail.com>
2025-11-14 23:17:15 -0600

    Commit

--- src/app/components/logos/logos.component.ts
+++ src/app/components/logos/logos.component.ts
@@ -1,11 +1,75 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logos',
-  imports: [],
   templateUrl: './logos.component.html',
-  styleUrl: './logos.component.css'
+  styleUrls: ['./logos.component.css']
 })
 export class LogosComponent {
 
+  constructor(private router: Router) { }
+
+  rutasInternas: { [key: string]: string } = {
+    "inicio": "/home",
+    "presentacion del programa": "/presentacion",
+    "presentacion programa": "/presentacion",
+    "presentacion": "/presentacion",
+    "objetivos": "/objetivos",
+    "mision y vision": "/misionVision",
+    "mision": "/misionVision",
+    "vision": "/misionVision",
+    "estado del arte": "/estadoArte",
+    "estado arte": "/estadoArte",
+    "infraestructura cientifica": "/infraestructura",
+    "infraestructura": "/infraestructura",
+    "nucleo academico basico": "/nucleoBasico",
+    "nucleo academico": "/nucleoBasico",
+    "nucleo basico": "/nucleoBasico",
+    "nucleo": "/nucleoBasico",
+    "lgyac": "/lgyac",
+    "lgac": "/lgyac",
+    "localizacion": "/localizacion",
+    "localizacion del programa": "/localizacion",
+    "sintesis del plan de estudios": "/sintesisPlan",
+    "sintesis": "/sintesisPlan",
+    "perfil de ingreso y egreso": "/ingresoEgreso",
+    "perfil de ingreso": "/ingresoEgreso",
+    "perfil de egreso": "/ingresoEgreso",
+    "requisitos de ingreso": "/requisitosIngreso",
+    "requisitos ingreso": "/requisitosIngreso",
+    "requisitos de permanencia": "/requisitosPermanencia",
+    "requisitos permanencia": "/requisitosPermanencia",
+    "requisitos de grado academico": "/requisitosAcademico",
+    "requisitos de grado": "/requisitosAcademico",
+    "requisitos grado": "/requisitosAcademico",
+    "productividad": "/productividad",
+    "egresados": "/egresados",
+    "cohorte generacional": "/cohorte",
+    "cohorte": "/cohorte",
+    "contacto": "/contacto"
+  };
+
+  // Método que se ejecuta al presionar Enter
+  onSearchKeyPress(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+
+      const input = event.target as HTMLInputElement;
+      const query = input.value.trim().toLowerCase();
+
+      if (query !== "") {
+        if (this.rutasInternas.hasOwnProperty(query)) {
+          this.router.navigate([this.rutasInternas[query]]);
+
+        } else {
+
+          window.open(
+            `https://www.itdurango.edu.mx/?q=${encodeURIComponent(query)}`,
+            "_blank"
+          );
+        }
+      }
+    }
+  }
 }
