ce2b742c00882a77dc963c53b2ed82f6ee1bbe73
rickymtx <rich.mtx1205@gmail.com>
2025-11-29 22:42:54 -0600

    Commit
    Funcionalidad del Navbar Tecno completada.

--- src/app/components/navbar-tec/navbar-tec.component.ts
+++ src/app/components/navbar-tec/navbar-tec.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-//import { RouterLink } from '@angular/router';
+import { Component, HostListener, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-tec',
-  //imports: [RouterLink],
   templateUrl: './navbar-tec.component.html',
-  styleUrl: './navbar-tec.component.css'
+  styleUrls: ['./navbar-tec.component.css']
 })
-export class NavbarTecComponent {
+export class NavbarTecComponent implements AfterViewInit {
 
+  private navbarMaestria!: HTMLElement | null;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.navbarMaestria = this.el.nativeElement.querySelector('.navbarMaestria');
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    if (!this.navbarMaestria) return;
+
+    const scrolled = window.scrollY > 70;
+
+    if (window.innerWidth > 768) {
+      if (scrolled) {
+        this.navbarMaestria.classList.add('fixed-navbar');
+      } else {
+        this.navbarMaestria.classList.remove('fixed-navbar');
+      }
+    } else {
+      this.navbarMaestria.classList.remove('fixed-navbar');
+    }
+  }
 }
